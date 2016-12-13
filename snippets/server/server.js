ed5c1e849054c35061e8b8b0b669b4079f64e8f7
Sean Guthrie (Purple Dog Enterprises) <seanpatrickguthrie@gmail.com>
2016-12-13 09:27:59 -0500

    Added interactivity with external services

--- snippets/server/server.js
+++ snippets/server/server.js
@@ -3,8 +3,15 @@ Meteor.publish('snippets', function(){
   //this.userId is the logged in user ID for the publish "session"
 });
 
+Meteor.publish('snippets-admin', function(){
+  return Snippets.find({});// we want ALL snippets;
+});
+
 Snippets.allow({
   insert: function(userId,fields){
-    return(userId);
+    return(userId); // make sure user is logged in.
+  },
+  update: function(userId,fields){
+    return(userId); // same as above - make sure user is logged in.
   }
 })
